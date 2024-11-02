b3f7aa19b0830464c8ffd6a274c785d3f95eb920
Nitecry7 <faheemfaizal@gmail.com>
2024-11-03 03:41:05 +0800

    Updated Profile.java, implemented a setUserPassWord() method for users to change from their default password, also implemented safety feature where if old password is not entered correctly, pw will not be updated and user has to re enter

--- Profile.java
+++ Profile.java
@@ -1,24 +1,43 @@
+import java.util.Scanner;
+
 public abstract class Profile 
 {
 
     public abstract void displayProfile(); //Various Profiles will implement their own version of this method, therefore I have set it as Abstract
 
-
+    private String hospitalID;
     private String emailAddress;
     private String userPassword;
     private String userRole;
 
-    public Profile(String emailAddress, String userPassword, String userRole) 
+    private static final String DEFAULT_PASSWORD = "I_l0ve_working_4_3ver";
+
+    public Profile(String hospitalID, String userPassword, String emailAddress, String userRole) 
     {
-        this.emailAddress = emailAddress;
+        this.hospitalID = hospitalID;
         this.userPassword = userPassword;
+        this.emailAddress = emailAddress;
         this.userRole = userRole;
+    }
+
+    public String getHospitalID() 
+    {
+        return hospitalID;
     }
 
     public String getEmailAddress()
     {
         return emailAddress;
     }
+
+    public String getPassword() {
+        return userPassword;
+    }
+
+    public void setPassword(String newPassword) {
+        this.userPassword = newPassword;
+    }
+
 
     public void setEmailAddress(String emailAddress)
     {
@@ -30,10 +49,31 @@ public abstract class Profile
         return userPassword;
     }
 
-    public void setUserPassword(String userPassword)
+    public void setUserPassword() 
     {
-        this.userPassword = userPassword;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("-----------Password Manager-----------");
+
+        System.out.println("Enter your old password: ");
+        String oldPassword = input.nextLine();
+
+        if(this.userPassword.equals(oldPassword)) //Added a security check where User has to enter old Password first before changing
+        {
+            System.out.println("Enter your new password: ");
+            String newPassword = input.nextLine();
+
+            this.userPassword = newPassword;
+
+            System.out.println("Password changed successfully.");
+        } 
+        else
+        {
+            System.out.println("Old password is incorrect. Password change failed. Try again.");
+            setUserPassword();
+        }
     }
+
 
     public String getUserRole()
     {
