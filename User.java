aa4a763f4cd4ac87e88bda4940eb802ccc072a3c
Nitecry7 <faheemfaizal@gmail.com>
2024-11-06 01:46:37 +0800

    Updated User Class to have display Message and a display Menu abstract class that other profiles will implement separately

--- User.java
+++ User.java
@@ -1,14 +1,17 @@
 public abstract class User extends Profile
 {
-    public abstract void displayProfile(); //Various Profiles will implement their own version of this method, therefore I have set it as Abstract
-
     public User(String hospitalID, String name, String emailAddress, String userRole)
     {
         super(hospitalID, name, emailAddress, DEFAULT_PASSWORD , userRole);
     }
+    
+    public abstract void displayMenu(); //Various Profiles will implement their own version of this method, therefore I have set it as Abstract
+
+  
 
     public void displayMessage()
     {
-        System.out.println("Welcome, " + getUserRole() + "!");
+        System.out.println("Welcome to the BrainRot Hospital!");
+        System.out.println("Welcome And Glad To See You " + getUserRole() + " " + getName() + "!");
     }
 }
